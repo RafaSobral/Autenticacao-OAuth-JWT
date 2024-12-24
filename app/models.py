0b5deb0af1ed2baa371718e8c35b89bd403c6ae7
Rafael Sobral <rafaelsobraldelimaalves@gmail.com>
2024-12-24 08:52:46 -0300

    Delete app directory

--- app/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-class Carros(models.Model):
-    modelo = models.CharField(max_length=50)
-    marca = models.CharField(max_length=50)
-    ano = models.IntegerField()
-
-
